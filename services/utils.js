eb2ce483011172001e9bebc5cd150b6a309d06e8
israel litvak <lisrael1989@gmail.com>
2024-09-27 14:26:39 +0300

    Added utility functions for game logic

---
+++ services/utils.js
@@ -0,0 +1,37 @@
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getRandomInt(min, max) {
+  if (Math.ceil(min) > Math.floor(max)) {
+    console.log('Next time try a bigger range');
+    return NaN;
+  } // add '+ 1'        HERE      to make it inclusive of max
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min)) + Math.ceil(min));
+}
+
+function startTimer() {
+  if (gTimerInterval) clearInterval(gTimerInterval);
+
+  gTimerInterval = setInterval(() => {
+    const timeDiff = Date.now() - startTime;
+    const time = getTime(timeDiff);
+
+    document.querySelector('span.seconds').innerText = time[0];
+    document.querySelector('span.milliseconds').innerText = time[1];
+  }, 10);
+}
+
+function getTime(timeDiff) {
+  const seconds = Math.floor(timeDiff / 1000);
+  const milliseconds = timeDiff % 1000;
+  return [(seconds + '').padStart(2, '0'), (milliseconds + '').padStart(3, '0')];
+}
+
+const zelda = new Audio('../img/botw.mp3');
+zelda.play();
